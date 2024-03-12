75ac8f69cc8a7cf3745875292ffafaea2da8c200
FlyntDenzel <flyntdenzel2004@gmail.com>
2024-03-12 10:53:34 +0100

    assigned various values to their states

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -9,15 +9,15 @@ const AddTask = () => {
     <form className="m-5 p-3  font-bold">
         <div className="grid pb-5">
             <label >Task</label>
-            <input type="text" placeholder="Add Task" className="border border-black p-2 mt-3  w-full"/>
+            <input type="text" placeholder="Add Task" className="border border-black p-2 mt-3  w-full" value={text} onChange={(e) => setText(e.target.value)}/>
         </div>
         <div className="grid mb-5">
             <label>Day & Time</label>
-            <input type="date" className="border border-black mt-3  p-2 "/>
+            <input type="date" className="border border-black mt-3  p-2 " value={day} onChange={(e) => setDay(e.target.value)}/>
         </div>
         <div className="flex justify-evenly">
             <label>Set Reminder</label>
-            <input type="checkbox" className="w-7"/>
+            <input type="checkbox" className="w-7" value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
         </div>
 
         <input type="submit" value='Save Task' className="bg-black text-white w-full p-1.5 rounded-md mt-4"/>
