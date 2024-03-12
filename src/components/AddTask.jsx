3c7b55520464d5565d6c7d52508717cd11752439
FlyntDenzel <flyntdenzel2004@gmail.com>
2024-03-12 10:47:12 +0100

    creating states for the various input forms

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react"
+
 const AddTask = () => {
+    const [text, setText] = useState('');
+    const [day, setDay] = useState('');
+    const [reminder, setReminder] = useState(false);
+
   return (
-    <form className="border m-5 p-3  font-bold">
+    <form className="m-5 p-3  font-bold">
         <div className="grid pb-5">
             <label >Task</label>
             <input type="text" placeholder="Add Task" className="border border-black p-2 mt-3  w-full"/>
